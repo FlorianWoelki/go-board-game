970965a2cc08fa05daf3cea42f9403e1e8b58b92
FlorianWoelki <FlorianWoelki@gmx.de>
2022-02-13 14:20:18 +0100

    feat: add intersection class

---
+++ src/components/Intersection.ts
@@ -0,0 +1,52 @@
+export class Intersection {
+  private x: number;
+  private y: number;
+  private value: string;
+
+  constructor(x: number, y: number, value: string = 'empty') {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+  }
+
+  duplicate(): Intersection {
+    const intersection = new Intersection(this.x, this.y, this.value);
+    return intersection;
+  }
+
+  isOccupiedWith(color: string): boolean {
+    if (this.isEmpty()) {
+      return false;
+    }
+
+    return this.value === color;
+  }
+
+  isEmpty(): boolean {
+    return this.value === 'empty';
+  }
+
+  setWhite(): void {
+    this.value = 'white';
+  }
+
+  setBlack(): void {
+    this.value = 'black';
+  }
+
+  setEmpty(): void {
+    this.value = 'empty';
+  }
+
+  isBlack(): boolean {
+    return this.value === 'black';
+  }
+
+  isWhite(): boolean {
+    return this.value === 'white';
+  }
+
+  sameColorAs(i: Intersection): boolean {
+    return i.value === this.value;
+  }
+}
